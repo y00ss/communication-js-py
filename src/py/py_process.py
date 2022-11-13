fe28e07b94ed81cfd7d64ad2c00038d33524073b
y00ss <gitlab"1324>
2022-11-13 17:12:09 +0100

    py process

---
+++ src/py/py_process.py
@@ -0,0 +1,25 @@
+import sys
+import json
+import ast  #
+
+data_send = "Hello js, I'm py"
+
+# String #
+# input = sys.argv[1]
+# output = data_send
+# print(output)
+
+# List
+# input = ast.literal_eval(sys.argv[1])
+# output = input
+# output.append(data_send)
+# print(json.dumps(output))
+
+
+# OBJ
+input = ast.literal_eval(sys.argv[1])
+output = input
+output['py_data'] = data_send
+print(json.dumps(output))
+
+sys.stdout.flush()
